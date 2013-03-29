661708396bcc3db5dc7832b9e26382da9d384982
Christian Autermann <christian@autermann.org>
2013-03-29 03:50:54 +0100

    moved stream reader

---
+++ src/main/java/org/n52/sos/sir/xml/StreamReader.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2013
+ * by 52 North Initiative for Geospatial Open Source Software GmbH
+ *
+ * Contact: Andreas Wytzisk
+ * 52 North Initiative for Geospatial Open Source Software GmbH
+ * Martin-Luther-King-Weg 24
+ * 48155 Muenster, Germany
+ * info@52north.org
+ *
+ * This program is free software; you can redistribute and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; even without the implied
+ * WARRANTY OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program (see gnu-gpl v2.txt). If not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
+ * visit the Free Software Foundation web page, http://www.fsf.org.
+ */
+
+package org.n52.sos.sir.xml;
+
+import javax.xml.stream.XMLEventFactory;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TODO JavaDoc
+ * @author Christian Autermann <c.autermann@52north.org>
+ */
+public class StreamReader {
+    private static final Logger log = LoggerFactory.getLogger(StreamReader.class);
+    private final XMLEventFactory eventFactory = XMLEventFactory.newInstance();
+    private final XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+    private final XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+    private XMLEventReader r;
+
+    public void bla() {
+    }
+
+}
