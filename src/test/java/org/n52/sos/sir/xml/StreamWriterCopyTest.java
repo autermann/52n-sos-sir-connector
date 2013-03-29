31101dd7dadc7c3541b163cec9a1c84f956306ed
Christian Autermann <christian@autermann.org>
2013-03-29 16:53:25 +0100

    tests for stream writers

---
+++ src/test/java/org/n52/sos/sir/xml/StreamWriterCopyTest.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (C) 2013
+ * by 52 North Initiative for Geospatial Open Source Software GmbH
+ *
+ * Contact: Andreas Wytzisk
+ * 52 North Initiative for Geospatial Open Source Software GmbH
+ * Martin-Luther-King-Weg 24
+ * 48155 Muenster, Germany
+ * info@52north.org
+ *
+ * This program is free software; you can redistribute and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; even without the implied
+ * WARRANTY OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program (see gnu-gpl v2.txt). If not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
+ * visit the Free Software Foundation web page, http://www.fsf.org.
+ */
+package org.n52.sos.sir.xml;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.junit.Test;
+import org.n52.sos.sir.SirConstants;
+
+/**
+ * @author Christian Autermann <c.autermann@52north.org>
+ */
+public class StreamWriterCopyTest {
+
+    @Test
+    public void test1() throws XMLStreamException, UnsupportedEncodingException {
+        assertThat(toString(new CopyStreamWriter("\n  <r>\n    <a> \n </a>\n    <b> \n </b>\n  </r>")),
+                   is(equalTo("<?xml version='1.0' encoding='UTF-8'?><r><a> \n </a><b> \n </b></r>")));
+    }
+
+    @Test
+    public void test2() throws XMLStreamException, UnsupportedEncodingException {
+        assertThat(toString(new CopyStreamWriter("<a/>")),
+                   is(equalTo("<?xml version='1.0' encoding='UTF-8'?><a />")));
+    }
+
+    @Test
+    public void test3() throws XMLStreamException, UnsupportedEncodingException {
+        assertThat(toString(new CopyStreamWriter("<a>b</a>")),
+                   is(equalTo("<?xml version='1.0' encoding='UTF-8'?><a>b</a>")));
+    }
+
+    @Test
+    public void test4() throws XMLStreamException, UnsupportedEncodingException {
+        assertThat(toString(new CopyStreamWriter("<a><b/></a>")),
+                   is(equalTo("<?xml version='1.0' encoding='UTF-8'?><a><b /></a>")));
+    }
+
+    @Test
+    public void test5() throws XMLStreamException, UnsupportedEncodingException {
+        assertThat(toString(new CopyStreamWriter("<?xml version='1.0' encoding='UTF-8'?><a />")),
+                   is(equalTo("<?xml version='1.0' encoding='UTF-8'?><a />")));
+    }
+
+    @Test
+    public void test6() throws XMLStreamException, UnsupportedEncodingException {
+        assertThat(toString(new CopyStreamWriter("<?xml version='1.0' encoding='UTF-8'?>  <a />")),
+                   is(equalTo("<?xml version='1.0' encoding='UTF-8'?><a />")));
+    }
+
+    private String toString(StreamWriter w) throws XMLStreamException, UnsupportedEncodingException {
+        ByteArrayOutputStream s = new ByteArrayOutputStream();
+        w.write(s);
+        return s.toString(SirConstants.UTF8);
+    }
+
+    private class CopyStreamWriter extends SirStreamWriter {
+        private String in;
+
+        CopyStreamWriter(String in) {
+            this.in = in;
+        }
+
+        @Override
+        protected void write() throws XMLStreamException {
+            copy(in);
+        }
+    }
+
+}
---
+++ src/test/java/org/n52/sos/sir/xml/StreamWriterTest.java
@@ -0,0 +1,162 @@
+/**
+ * Copyright (C) 2013
+ * by 52 North Initiative for Geospatial Open Source Software GmbH
+ *
+ * Contact: Andreas Wytzisk
+ * 52 North Initiative for Geospatial Open Source Software GmbH
+ * Martin-Luther-King-Weg 24
+ * 48155 Muenster, Germany
+ * info@52north.org
+ *
+ * This program is free software; you can redistribute and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; even without the implied
+ * WARRANTY OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program (see gnu-gpl v2.txt). If not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
+ * visit the Free Software Foundation web page, http://www.fsf.org.
+ */
+package org.n52.sos.sir.xml;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.n52.sos.sir.SirConstants;
+
+/**
+ * @author Christian Autermann <c.autermann@52north.org>
+ */
+public class StreamWriterTest {
+    private static final int BUFFER_SIZE = 4096;
+    private static final String SOS_SENSOR_ID = "urn:ogc:object:feature:Sensor:EEA:airbase:7.0:FR17016";
+    private static final String SIR_SENSOR_ID = "urn:sir:FR17016";
+    private static final String SOS_SERVICE_URL = "http://localhost:8080/52n-sos-webapp/sos/kvp";
+    private String sensorDescription;
+
+    @Test
+    public void testSensorDescription() throws FileNotFoundException, XMLStreamException {
+        new SensorDescriptionStreamWriter()
+                .setSensorDescription(sensorDescription)
+                .write(new NullOutputStream());
+    }
+
+    @Test
+    public void testDeleteRequest() throws FileNotFoundException, XMLStreamException {
+        new DeleteRequestStreamWriter()
+                .setSosProcedureIdentifier(SOS_SENSOR_ID)
+                .setSirProcedureIdentifier(SIR_SENSOR_ID)
+                .setSosServiceUrl(SOS_SERVICE_URL)
+                .write(new NullOutputStream());
+    }
+
+    @Test
+    public void testUpdateRequest() throws XMLStreamException, FileNotFoundException {
+        new UpdateRequestStreamWriter()
+                .setSensorDescription(sensorDescription)
+                .setSirProcedureIdentifier(SIR_SENSOR_ID)
+                .write(new NullOutputStream());
+    }
+
+    @Test
+    public void testInsertRequest() throws FileNotFoundException, XMLStreamException {
+        new InsertRequestStreamWriter()
+                .setSosProcedureIdentifier(SOS_SENSOR_ID)
+                .setSensorDescription(sensorDescription)
+                .setSosServiceUrl(SOS_SERVICE_URL)
+                .write(new NullOutputStream());
+    }
+
+    @Test
+    public void testSensorDescriptionOutput() throws FileNotFoundException, XMLStreamException,
+                                                     UnsupportedEncodingException,
+                                                     IOException, TransformerConfigurationException,
+                                                     TransformerException {
+        String r = toString(new SensorDescriptionStreamWriter()
+                .setSensorDescription(sensorDescription));
+        String expected = readFile("/SensorDescription.xml");
+        assertThat(r, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testDeleteRequestOutput() throws FileNotFoundException, XMLStreamException, UnsupportedEncodingException,
+                                                 IOException, TransformerConfigurationException, TransformerException {
+        String r = toString(new DeleteRequestStreamWriter()
+                .setSosProcedureIdentifier(SOS_SENSOR_ID)
+                .setSirProcedureIdentifier(SIR_SENSOR_ID)
+                .setSosServiceUrl(SOS_SERVICE_URL));
+        String expected = readFile("/delete.xml");
+        assertThat(r, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testUpdateRequestOutput() throws XMLStreamException, FileNotFoundException, UnsupportedEncodingException,
+                                                 IOException, TransformerConfigurationException, TransformerException {
+        String r = toString(new UpdateRequestStreamWriter()
+                .setSensorDescription(sensorDescription)
+                .setSirProcedureIdentifier(SIR_SENSOR_ID));
+        String expected = readFile("/update.xml");
+        assertThat(r, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testInsertRequestOutput() throws FileNotFoundException, XMLStreamException, UnsupportedEncodingException,
+                                                 IOException, TransformerConfigurationException, TransformerException {
+        String r = toString(new InsertRequestStreamWriter()
+                .setSosProcedureIdentifier(SOS_SENSOR_ID)
+                .setSensorDescription(sensorDescription)
+                .setSosServiceUrl(SOS_SERVICE_URL));
+        String expected = readFile("/insert.xml");
+        assertThat(r, is(equalTo(expected)));
+    }
+
+    private String toString(final StreamWriter w) throws XMLStreamException, UnsupportedEncodingException,
+                                                         TransformerConfigurationException, IOException, TransformerException {
+        ByteArrayOutputStream s = new ByteArrayOutputStream();
+        w.writePretty(s);
+        return s.toString(SirConstants.UTF8);
+    }
+
+    @Before
+    public void setUp() throws IOException {
+        this.sensorDescription = readFile("/SensorDescription.xml");
+    }
+
+    protected String readFile(String file) throws IOException {
+        BufferedInputStream in = null;
+        ByteArrayOutputStream out = null;
+        try {
+            in = new BufferedInputStream(StreamWriterTest.class.getResourceAsStream(file));
+            out = new ByteArrayOutputStream();
+            byte[] buf = new byte[BUFFER_SIZE];
+            int l;
+            while ((l = in.read(buf)) > 0) {
+                out.write(buf, 0, l);
+            }
+            return out.toString(SirConstants.UTF8);
+        } finally {
+            IOUtils.closeQuietly(in);
+            IOUtils.closeQuietly(out);
+        }
+    }
+
+    
+}
